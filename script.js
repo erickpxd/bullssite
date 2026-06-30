062bdb651763f8ef7329379ca3c434a082b365ca
erickpxd <erick.monteiroeflm@gmail.com>
2026-06-30 13:05:06 -0300

    Add mobile menu toggle

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        const menuIsOpen = $('.navbar .menu').hasClass("active");
+
+        $(this).attr("aria-expanded", menuIsOpen);
+        $(this).find(".menu-icon").html(menuIsOpen ? "&times;" : "&#9776;");
+    });
+
+    $('.navbar .menu a').click(function(){
+        $('.navbar .menu').removeClass("active");
+        $('.menu-btn').attr("aria-expanded", "false");
+        $('.menu-btn .menu-icon').html("&#9776;");
+    });
 });
